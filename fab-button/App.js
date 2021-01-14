2fae9236e4c57ed1e73f911487cef853c477e301
viniciusiess <siess.vinicius@gmail.com>
2021-01-14 00:45:45 -0300

    Primeiro commit

---
+++ fab-button/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { StyleSheet, View } from 'react-native'
+import FabButton from './src/components/FabButton'
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <FabButton 
+        style={{ bottom: 80, right: 60 }}
+      />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex:1,
+    backgroundColor: '#fff'
+  }
+})
